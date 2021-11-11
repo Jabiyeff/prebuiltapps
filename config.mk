37818e500b84fbf38a914743c70d1e408dfb1855
Jabiyeff <cebiyevanar@gmail.com>
2021-11-12 00:48:31 +0400

    Remove lawnchair and import google files go
    Signed-off-by: Jabiyeff <cebiyevanar@gmail.com>

--- config.mk
+++ config.mk
@@ -1,5 +1,8 @@
 PRODUCT_SOONG_NAMESPACES += \
     vendor/prebuilt-apps
 
+PRODUCT_COPY_FILES += \
+    vendor/prebuilt-apps/privapp-permissions-google-nbu.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-google-nbu.xml
+
 PRODUCT_PACKAGES += \
-    Lawnchair
+    FilesPrebuilt
